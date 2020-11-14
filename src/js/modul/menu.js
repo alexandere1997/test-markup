a522bc09d492205c5407e5d789cd6208501adb9e
Alexander <Sanyi.Ponzel@mail.ru>
2020-11-14 21:59:25 +0700

    test1

--- src/js/modul/menu.js
+++ src/js/modul/menu.js
@@ -14,7 +14,7 @@ let menu = () => {
     close.addEventListener("click", () => {
         nav__box.style.transform = "translateY(-385px)"
         setTimeout(() => {
-            menu.style.transform = "translateY(-439px)"
+            menu.style.transform = "translateY(-539px)"
         }, 700)
     });
 }
